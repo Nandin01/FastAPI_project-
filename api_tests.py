3312eec190af09397c1c29c2c4b754404413df4d
Nandin Ganbat <95683647+Nandin01@users.noreply.github.com>
2023-02-01 16:39:06 +0100

    Add: unittest sample

---
+++ api_tests.py
@@ -0,0 +1,12 @@
+from fastapi.testclient import TestClient
+from main import app
+
+
+        # def test_index(self):
+        #     response = client.get("/")
+        #     self.assertEqual(response.status_code, 200)
+        #     # assert response.
+        #
+        # def test_list_runs(self):
+        #     response = client.get("/all_run_data")
+        #     self.assertTrue(response.ok)
